7c6c63bd0a87423d167f2f516e21baf0be5675eb
1995Tangerine <1995Tangerine@msn.cn>
2019-09-01 23:23:09 -0400

    Update example_dpeak.cpp

--- example/example_dpeak.cpp
+++ example/example_dpeak.cpp
@@ -1,9 +1,8 @@
-
 #include <random>
 #include <iostream>
 #include "dpeak.h"
 
-
+// Inverse Error Function
 float myErfInv2(float x) {
 	float tt1, tt2, lnx, sgn;
 	sgn = (x < 0) ? -1.0f : 1.0f;
@@ -49,28 +48,31 @@ int main() {
 
 
 
-	//Calculate the probability of being a misidentified bead at the values of all reads. 
+	// Calculate the probability of being a misidentified bead at the values of all reads. 
 	std::vector<float> probbg;
 	float pdf_bg = 1. / 12;				// PDF of uniform distribution(2.0,14.0) is a constant 
 
 	for (int i = 0; i < n_reads; i++)
 	{
-		probbg.push_back(pdf_bg);		//pdf_bg will be replaced by the value of pdf_bg at the value of eachread
+		probbg.push_back(pdf_bg);		// pdf_bg will be replaced by the value of pdf_bg at the value of each read
 	}
 
 
 	// The grid of all possible peak locations for likelihood calculation.
-	std::vector<float> peakgrid;
-	for (float i = 2.0; i < 14.0; i = i + 0.04) { peakgrid.push_back(i); }
-	int gridsize = peakgrid.size();
+	float grid_start = 2.0;
+	float grid_end = 14.0;
+	float grid_step = 0.04;
 
+	std::vector<float> peakgrid;
+	for (float i = grid_start; i < grid_end; i = i + grid_step) { peakgrid.push_back(i); }
+	int gridsize = peakgrid.size();
 
 
 
 	float dp52_ratio = 2. / 3;						// ratio of dp52 beads
 	float bg = 0.04;								// alpha_c in the paper
 
-	//Calculate the log-likelihood function. 
+	// Calculate the log-likelihood function. 
 	std::vector<float> likelihood = dpeak_single(dp52_ratio, bg, values, probbg, peakgrid);
 	float max_likelihood = likelihood[0];
 	for (auto l : likelihood)
@@ -83,7 +85,7 @@ int main() {
 		l -= max_likelihood;
 	}
 
-	//Marginal likelihood distribution
+	// Marginal likelihood distribution
 	std::vector<float> marginal_hi(gridsize, 0.0);
 	std::vector<float> marginal_lo(gridsize, 0.0);
 
@@ -96,18 +98,12 @@ int main() {
 		}
 	}
 
-	for (std::vector<float>::const_iterator i = marginal_hi.begin(); i != marginal_hi.end(); ++i) { std::cout << *i << ' '; }
-	std::cout << std::endl;
 
-	for (std::vector<float>::const_iterator i = marginal_lo.begin(); i != marginal_lo.end(); ++i) { std::cout << *i << ' '; }
-	std::cout << std::endl;
-
-	//normalization
+	// Normalization
 	float sum_hi = 0;
 	for (auto& n : marginal_hi) sum_hi += n;
 	for (auto& n : marginal_hi) n = n / sum_hi;
 
-	std::cout << sum_hi << std::endl;
 
 	float sum_lo = 0;
 	for (auto& n : marginal_lo) sum_lo += n;
@@ -116,33 +112,31 @@ int main() {
 
 	// Z-score inference
 
-	// The reference is the distribution of reads without perturbation, here we use a mock distribution 
+	// In real data, the reference is the distribution of reads from the same gene without perturbation 
+	// Here we use a normal distribution (9,1)
 	std::vector<float> ref(gridsize, 0.0);
 	for (float i = 0; i < gridsize; i = i++)
 	{
 		float x = 2 + 0.04*i - 9;
-		ref[i] = 0.5 * erfc(-x * sqrt(0.5)) - 0.5 * erfc((-x + 0.04) * sqrt(0.5));
+		ref[i] = 0.5 * erfc(-x * sqrt(0.5)) - 0.5 * erfc((-x + grid_step) * sqrt(0.5));
 	}
 	float sum_ref = 0;
 	for (auto& n : ref) sum_ref += n;
 	for (auto& n : ref) n = n / sum_ref;
 
-	for (std::vector<float>::const_iterator i = ref.begin(); i != ref.end(); ++i) { std::cout << *i << ' '; }
-	std::cout << std::endl;
 
-	float q1 = 0.0;									//Marginal quantile 
-
+	// Calculate z-score for two marginal distributions with ref
+	float q1 = 0.0;									
 	for (float i = 0; i < gridsize; i = i++)
 	{
 		float sum_temp = 0;
 		for (int j = i; j < gridsize; j++) { sum_temp += marginal_hi[j]; }
 		q1 += ref[i] * sum_temp;
 	}
-
 	float z1 = sqrt(2) * myErfInv2(2 * q1 - 1);
 
-	float q2 = 0.0;
 
+	float q2 = 0.0;
 	for (int i = 0; i < gridsize; i++)
 	{
 		float sum_temp = 0;
@@ -151,8 +145,8 @@ int main() {
 	}
 	float z2 = sqrt(2) * myErfInv2(2 * q2 - 1);
 
-
-	std::cout << z1 << " " << z2 << std::endl;
+	// z-scores 
+	std::cout << "z_hi=" << z1 << "  " << "z_lo=" << z2 << std::endl;
 	system("pause");
 	return 0;
 }
